4554fa7c4ab750520fd83e99d0f6a33225c57912
Naseem Machado <naseemmachado@gmail.com>
2020-06-29 02:37:41 -0400

    Registration valid form

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 
-let exphbs= require("express-handlebars");
+const exphbs= require("express-handlebars");
 const multer = require("multer");
 
 const path = require("path");
 
-let bodyparser = require("body-parser");
+const bodyparser = require("body-parser");
 app.use(bodyparser.urlencoded({ extended: false }));
 
 app.use(express.static("Public"));
@@ -50,11 +50,93 @@ app.get("/registration",(req,res)=>{
      title:"Registration"
 
     });
-    
-    //Validation
-
-
 });
+
+    app.post("/registration",(req,res)=>{
+    //Validation
+    const errorMessage_name=[];
+    const errorMessage_email=[];
+    const errorMessage_password=[];
+    const errorMessage_password2=[];
+   
+    if(req.body.username == "") {
+         errorMessage_name.push('*Enter name');
+    }
+    if(req.body.password == ""){
+        errorMessage_password.push('*Enter password');
+    }
+    if(req.body.email == "") {
+        errorMessage_email.push('*Enter an email address');
+    }
+    else
+    {
+        if(req.body.password.length < 6){
+            errorMessage_password.push('*Password must be at least 6 charactets long');
+        }
+        else if(req.body.password.length > 20){
+            errorMessage_email.push('*password must not contain more than 20 characters')
+        }
+        else if(password_Validate(req.body.password)==false){
+            errorMessage_password.push('*Password should must have letters and numbers only!');
+        }
+        
+    }
+    if(req.body.password2 == ""){
+        errorMessage_password2.push('*Re-enter password!');
+    }
+    if(req.body.password2 != req.body.password){
+        errorMessage_password2.push('*Passwords do not match');
+    }   
+
+//password regex
+function password_Validate(str) {
+    const pattern = new RegExp(/^[0-9a-zA-Z]+$/);
+    return pattern.test(str);
+}
+//failed validation
+   //if (errorMessage_email.length > 1 || errorMessage_name.length > 1 || errorMessage_password.length > 1 || errorMessage_password2.length >1){
+        res.render('registration', {
+            layout: false,
+            title:"Registration failed",
+            e_Name: errorMessage_name,
+            e_Email: errorMessage_email,
+            e_Password: errorMessage_password,
+            e_Password2: errorMessage_password2,
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            password2: req.body.password2
+        });
+//}
+
+ if(errorMessage_name.length == 0 && errorMessage_email.length == 0 && errorMessage_password.length == 0 && errorMessage_password2.length == 0)
+            {
+                        const newUser =
+                        {
+                            Name: req.body.username,
+                            email: req.body.email,
+                            password: req.body.password
+                        };
+                const sgMail = require('@sendgrid/mail');
+                sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
+                const msg = {
+                    to: `${newUser.email}`,
+                    from: `@gmail.com`,
+                    subject: `Welcome to FoodNow!`,
+                    html:
+                       `<strong>${newUser.username} <br>
+                    Vistor's Email Address ${newUser.email} <br>
+                    Welcome to FoodNow. Your have successfully signed up!<br></strong>
+                   `,
+                };
+                    sgMail.send(msg)
+                    .then(() => {
+                        res.redirect("/home");
+                    })
+                    }
+                                      
+});
+
 
 app.get("/login",(req,res)=>{
     res.render('login', {
@@ -63,6 +145,14 @@ app.get("/login",(req,res)=>{
     });
 
 });
+app.post("/login",(req,res)=>{
+    res.render('login', {
+        layout: false,
+        title:"login"
+    });
+
+});
+
 
 app.get("/mealPackage",(req,res)=>{
 
